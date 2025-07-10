8138eceecba6e686ca19ed95152896c49e5289ea
Division1705 <division.pvt06@gmail.com>
2025-07-10 21:29:14 +0530

    feat:adding /signup route

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectDB } = require('./src/configs/database.js')
+const User = require('./src/models/users.model.js')
 
 const app = express()
 
@@ -7,6 +8,22 @@ const PORT = 8000
 
 app.use(express.json())
 app.use(express.urlencoded(true))
+
+app.post('/signup',async(req,res) =>{
+    const user = new User(req.body)
+    try {
+      await user.save()
+      console.log('Registration Sucessfull')
+      res.status(200).json({
+        sucess:true,
+        message:'Registration sucessfull'
+      })  
+    } catch (error) {
+        res.status(400).json({
+            sucess:false,
+            message:'Not able to register'
+        })}
+})
 
 
 connectDB().then(() => {
