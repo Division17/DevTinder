a0f46a21951bf8cff510108faf84bb2098ebbbf6
Division1705 <division.pvt06@gmail.com>
2025-07-11 11:37:19 +0530

    feat:validation in userSchema

--- Backend/src/models/users.model.js
+++ Backend/src/models/users.model.js
@@ -3,22 +3,50 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        min:18,
+        max:100 
     },
     gender: {
         type: String,
+        validate(value){
+            if (!["male","female", "others"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
     },
+    photoUrl: {
+        type: String,
+        default: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/626fd8140423801.6241b91e24d9c.png'
+    },
+    about: {
+        type: String,
+        default: 'This is a default about of the user!!'
+    },
+
+    skills: {
+        type: [String],
+    }
+
 })
 
 module.exports = mongoose.model('User', userSchema)
