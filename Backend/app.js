48ce216f692059ef3c895d1f5e01620737b2c9b3
Division1705 <division.pvt06@gmail.com>
2025-07-12 00:10:04 +0530

    feat:add token and cookie logic

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,8 @@ const { connectDB } = require('./src/configs/database.js')
 const User = require('./src/models/users.model.js')
 const { validateSignUp, validateLogin } = require('./src/utility/Validations.js')
 const bcrypt = require('bcryptjs')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
@@ -10,6 +12,7 @@ const PORT = 8000
 
 app.use(express.json())
 app.use(express.urlencoded(true))
+app.use(cookieParser())
 
 app.get('/feed', async (req, res) => {
     try {
@@ -25,6 +28,32 @@ app.get('/feed', async (req, res) => {
         })
 
         console.log(error)
+    }
+})
+
+app.get('/profile', async (req, res) => {
+    try {
+        const token = req.cookies.token
+        if (!token) {
+            throw new Error("User not authenticated")
+        } else {
+          const decode = await jwt.verify(token,"secrect key 123321")
+          const user = await User.findById(decode.userId)
+          if(!user){
+            throw new Error("User not found")
+          }else{
+            res.status(200).json({
+                success:true,
+                message:"user found",
+                data:user
+            })
+          }
+        }
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
     }
 })
 
@@ -65,7 +94,7 @@ app.post('/login', async (req, res) => {
         validateLogin(req)
         const { emailId, password } = req.body;
 
-        const user = await User.findOne({emailId})
+        const user = await User.findOne({ emailId })
         if (!user) {
             res.status(400).json({
                 success: false,
@@ -74,18 +103,20 @@ app.post('/login', async (req, res) => {
         }
 
         const checkPassword = await bcrypt.compare(password, user.password)
-        if (!checkPassword) {
+        if (checkPassword) {
+            const token = await jwt.sign({userId:user._id}, "secrect key 123321")
+            res.cookie("token", token)
+            res.status(200).json({
+                success: true,
+                message: 'Login successful'
+            })
+        }
+        else {
             res.status(400).json({
                 success: false,
                 message: 'credentials are wrong.'
             })
         }
-
-        res.status(200).json({
-            success:true,
-            message:'Login successful'
-        })
-
     } catch (error) {
         res.status(400).json({
             success: false,
@@ -108,18 +139,17 @@ app.delete("/user", async (req, res) => {
     }
 })
 
+//fix it later
+
 app.patch('/user', async (req, res) => {
     const data = req.body;
     const userId = data.userId;
     try {
         const allowed_updates = ["photoUrl", "about", "age", "gender", "skills"]
-
         const isUpdateAllowed = Object.keys(data).every((k) => allowed_updates.includes(k))
-
         if (!isUpdateAllowed) {
             throw new Error("Update is not allowed")
         }
-
         if (data.skills.length > 10) {
             throw new Error("Skills can not be more than 10")
         }
@@ -135,7 +165,7 @@ app.patch('/user', async (req, res) => {
 })
 
 connectDB().then(() => {
-    console.log('Connection Establisher')
+    console.log('Connection Established')
     app.listen(PORT, () => {
         console.log("Server is Running")
     })
--- Backend/src/configs/database.js
+++ Backend/src/configs/database.js
@@ -4,8 +4,6 @@ const connectDB = async()=>{
 
     try {
         await mongoose.connect('mongodb://localhost:27017/devTinder')
-
-        console.log("Connected sucessfully")
     } catch (error) {
         console.log(error)
     }
