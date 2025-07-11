54b6c2e19ba3a3f6c8ce248c1894862306fd6250
Division1705 <division.pvt06@gmail.com>
2025-07-11 11:20:53 +0530

    feat: add user update route

--- Backend/app.js
+++ Backend/app.js
@@ -48,15 +48,29 @@ app.delete("/user", async (req, res) => {
     try {
         const user = await User.findByIdAndDelete({ _id: userId });
         res.status(200).json({
-            success:true,
-            message:'User deleted sucessfully'
+            success: true,
+            message: 'User deleted sucessfully',
+            data:user
         })
     } catch (error) {
-     res.status(400).send("Something went wrong")
+        res.status(400).send("Something went wrong")
     }
 })
 
-
+app.patch('/user', async (req, res) => {
+    const data = req.body;
+    const userId = data.userId;
+    try {
+        await User.findByIdAndUpdate(userId , data, {returnDocument:"after"})
+        res.status(200).json({
+            success: true,
+            message: 'User updated sucessfully'
+        })
+    } catch (error) {
+        res.status(400).send("Something went wrong")
+        console.log(error)
+    }
+})
 
 connectDB().then(() => {
     console.log('Connection Establisher')
